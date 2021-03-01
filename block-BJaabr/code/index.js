945349b1a570d518b96a8916b34e8e1959b57fe2
farhaneas11 <farhan.salam11@gmail.com>
2021-03-01 13:46:03 +0530

    js2

--- block-BJaabr/code/index.js
+++ block-BJaabr/code/index.js
@@ -7,25 +7,30 @@ Do the following for the given problem:
 */
 
 // - Write a Function Decleration
+
 function convertToString(n) {
   return String(n);
 }
+convertToString(21);
 
 // - Write a Function Expression
-let convertToString = function (n) {
+
+let convertToString1 = function (n) {
   return String(n);
 };
+convertToString1(21);
 
 // - Write an Arrow Function without curly brackets (if possible)
-let convertToString = (n) => String(n);
-
+let convertToString2 = (n) => String(n);
+convertToString2(22);
 // - Write an Arrow Function with curly brackets
-let convertToString = (n) => {
+let convertToString3 = (n) => {
   return String(n);
 };
-
+convertToString3(21);
 // - Execute the function
-convertToString(21);
+/*
+convertToString4(21);*/
 
 // - Execute the function and store the return value in a variable.
 let returnValue = convertToString(23);
@@ -41,19 +46,26 @@ Do the following with above problem.
 */
 
 // - Write a Function Declaration
-
+function addOne(num){
+  return`${num + 1}`;
+}
+addOne(20);
 // - Write a Function Expression
 
 // - Write an Arrow Function without curly brackets(if possible)
+let addOn=function(num1) {
+  return`${num1 + 1}`;
+}
 
 // - Write an Arrow Function with curly brackets
-
+let addonn = (num2) =>  num2 + 1 ;
 // - Execute the function
-
+addonn(20);
 // - Execute the function and store the return value in a variable.
-
+let addvar =addOn(20);
+alert (addvar);
 // - What is the typeof returnValue
-
+typeof addOnn; //function
 /* 2. Subtract one
 
 Write a function named `substractOne` that accepts a number, substracts one from the given value and return it. (hint: use return keyword)
@@ -62,19 +74,24 @@ Do the following with above problem.
 */
 
 // - Write a Function Declaration
-
+function substractOne(sub){
+  return`${sub - 1}`;
+}
+substractOne(20);
 // - Write a Function Expression
 
 // - Write an Arrow Function without curly brackets(if possible)
-
+let subOn=function(sub1) {
+  return`${sub1 - 1}`;
+}
 // - Write an Arrow Function with curly brackets
-
+let subonn = (sub2) =>  sub2 - 1 ;
 // - Execute the function
 
 // - Execute the function and store the return value in a variable.
 
 // - What is the typeof returnValue
-
+typeof subOnn;//function
 /* 3. Add two numbers
 
 Write a function named `sum` that accepts two numbers and returns the sum of them.
@@ -149,11 +166,26 @@ Do the following with above problem.
 */
 
 // - Write a Function Declaration
-
+function oddOrEven(ore){
+  if(ore%2==0){
+    return`even`;
+  }
+  else {
+    return`odd`;
+  }  
+}
 // - Write an anonymous Function Expression
-
+let oddorEven = (ore1) => ore1%2==0 ;'even';
+oddorEven(20);
 // - Write an named Function Expression
-
+let oddorEven = function oddOrEven(ore){
+  if(ore%2==0){
+    return`even`;
+  }
+  else {
+    return`odd`;
+  }  
+}
 // - Write an Arrow Function without curly brackets (hint: use ternary operator)
 
 // - Write an Arrow Function with curly brackets
@@ -161,5 +193,6 @@ Do the following with above problem.
 // - Execute the function
 
 // - Execute the function and store the return value in a variable
-
+let oddeven = oddOrEven(21);
 // - What is the typeof returnValue
+typeof oddOrEven;//function
